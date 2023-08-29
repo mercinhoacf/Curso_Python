1bfcfeef35ae0c8b1d30cbc366043875e003210f
Emerson Aquino <130510634+mercinhoacf@users.noreply.github.com>
2023-08-29 17:42:28 -0300

    Create aula8.py

---
+++ aula8.py
@@ -0,0 +1,13 @@
+nome = 'Emerson'
+sobrenome = 'Aquino'
+idade = 20
+ano_de_nascimento = 2023 - idade
+maior_de_idade = idade >=18
+altura = 1.81
+
+print ('Nome:',nome)
+print ('Sobrenome:',sobrenome)
+print ('Idade:',idade)
+print ('Nasceu em que ano?:',ano_de_nascimento)
+print ('É maior de idade?:',maior_de_idade)
+print ('Altura:',altura)
