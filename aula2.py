33d81655aecec3eaa2ac315641b643c287e08ce4
Emerson Aquino <130510634+mercinhoacf@users.noreply.github.com>
2023-08-29 17:40:33 -0300

    Create aula2.py

---
+++ aula2.py
@@ -0,0 +1,2 @@
+print (12, 34, sep='-', end='\n##') #serve como um separador nos espaços em branco// o end serve p adicionar algo no final da impressao, para adicionar quebra de linha use \n
+print (56, 78, sep='-', end='\n') #serve como um separador nos espaços em branco
