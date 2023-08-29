fc3eea64bb21958be4c405c2cd18274a07d4ebf6
Emerson Aquino <130510634+mercinhoacf@users.noreply.github.com>
2023-08-29 17:43:48 -0300

    Create aula12.py

---
+++ aula12.py
@@ -0,0 +1,14 @@
+#IMC = peso / (altura*altura)
+
+nome = 'Emerson Aquino'
+idade = 20
+altura = 1.81
+peso = 82
+imc = peso / (altura * altura)
+print (imc)
+
+print (nome, 'tem', altura, 'de altura', 'pesa', peso, 'e seu IMC é de', imc)
+
+#Emerson Aquino tem 1.81 de altura,
+#pesa 82kg e seu IMC é 
+#25.029760996306585
