bd696912fcfb0b65e4f83efa76680e1403cdf602
Emerson Aquino <130510634+mercinhoacf@users.noreply.github.com>
2023-08-29 17:44:52 -0300

    Create aula16.py

---
+++ aula16.py
@@ -0,0 +1,8 @@
+entrada = input ('Você quer "entrar" ou "sair"?')
+
+if entrada == 'entrar':
+    print ('Você conseguiu acessar o sistema!!:)')
+elif entrada == 'sair':
+    print ('Você saiu do sistema!!:(')
+else:
+    print ('Você não digitou nem "entrar" nem "sair"')     
