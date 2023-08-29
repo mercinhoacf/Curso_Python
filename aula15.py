c59d646a5221439346e9dede2403ba9f10a2bf03
Emerson Aquino <130510634+mercinhoacf@users.noreply.github.com>
2023-08-29 17:44:35 -0300

    Create aula15.py

---
+++ aula15.py
@@ -0,0 +1,12 @@
+''''''
+nome = input ('Qual seu nome?')
+numero = input ('Digite um numero:')
+numero_2 = input ('Digite outro numero:')
+
+int_numero = int (numero)
+int_numero_2 = int (numero_2)
+print (f'Seu nome é: {nome}')
+print (f'O primeiro numero que voce digitou foi: {int_numero}')
+print (f'O segundo numero que voce digitou foi: {int_numero_2}')
+print (f'A soma dos numeros digitados é: {int_numero + int_numero_2}')
+''''''
