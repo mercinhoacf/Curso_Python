567d0432a4735729231018615662ed6d45d7a067
Emerson Aquino <130510634+mercinhoacf@users.noreply.github.com>
2023-08-29 17:42:13 -0300

    Create aula7.py

---
+++ aula7.py
@@ -0,0 +1,15 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+# PEP8: inicie variáveis com letras minúsculas, pode usar
+# números e underline _.
+# O sinal de = é o operador de atribuição. Ele é usado para
+# atribuir um valor a um nome (variável).
+# Uso: nome_variavel = expressão
+
+#nome_completo = 'Emerson '
+#print (nome_completo)
+
+nome = 'pp'
+idade = 20
+maior_de_idade = idade >= 18
+print ('Nome: ', nome, 'Idade: ', idade)
+print ('É maior?: ', maior_de_idade)
