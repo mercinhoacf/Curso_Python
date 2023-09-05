7159dc58a3582eabe5696317d05e54024a3a55e9
Emerson Aquino <130510634+mercinhoacf@users.noreply.github.com>
2023-09-05 17:40:10 -0300

    Create aula33.py

---
+++ aula33.py
@@ -0,0 +1,9 @@
+"""
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutáveis que vimos: str, int, float, bool
+"""
+string = 'emerson aquino'
+outra_variavel = f'{string[:3]}SPORT{string[4:]}'
+print(string.capitalize())
+print(outra_variavel)
+#print(string.capitalize())
