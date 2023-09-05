23c0b2fb5ec3b0d7b95617fa67e65873cadd7e21
Emerson Aquino <130510634+mercinhoacf@users.noreply.github.com>
2023-09-05 17:42:02 -0300

    Create aula38.py

---
+++ aula38.py
@@ -0,0 +1,13 @@
+qtd_linhas = 7
+qtd_colunas = 7
+
+linha = 1
+
+while linha <= qtd_linhas:
+    coluna = 1 
+    while coluna <= qtd_colunas:
+        print (f'{linha=} {coluna=}')
+        coluna += 1 
+    linha += 1
+
+print ('its over')
