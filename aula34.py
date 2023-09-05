34445c095a75fec86eb5346f22067997f20a043b
Emerson Aquino <130510634+mercinhoacf@users.noreply.github.com>
2023-09-05 17:40:30 -0300

    Create aula34.py

---
+++ aula34.py
@@ -0,0 +1,11 @@
+condicao = True
+
+while condicao:
+    nome = input ('Qual seu nome?')
+    print (f'Prazer em te conhecer, {nome} :)')
+
+    if nome == 'sair':
+       
+        break 
+
+print ('Cabô')
