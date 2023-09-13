cf85401221bf6a13f51838178beed819376a29d6
Emerson Aquino <130510634+mercinhoacf@users.noreply.github.com>
2023-09-13 17:31:46 -0300

    Create aula44.py

---
+++ aula44.py
@@ -0,0 +1,24 @@
+# numeros = range(10)
+
+# for numero in numeros:
+#     print (numero)
+
+
+# numeros = range(1, 16, 2)
+
+# for numero in numeros:
+#     print (numero)
+
+
+# numeros = range(0, -10, -1)
+
+# for numero in numeros:
+
+#     print (numero)
+
+
+numeros = range(0, 10, 2) #para saber numero pares de 10 (se quiser saber de outro numero basta mudar o 10)
+
+for numero in numeros:
+
+    print (numero)
