0635be6abd41ab3cd5e11f12cdf0cbd1f7cd3a13
Emerson Aquino <130510634+mercinhoacf@users.noreply.github.com>
2023-10-16 18:47:08 -0300

    Create aula48_1.py

---
+++ aula48_1.py
@@ -0,0 +1,10 @@
+lista = [10, 20, 30, 40]
+lista [2] = 'emerson'
+del lista[3] #excluir o elemento 3 da lista
+print(lista)
+print(lista[2])
+lista.append (50) #add um elemento no final da lista.
+lista.append (60) #add um elemento no final da lista.
+lista.append (70) #add um elemento no final da lista.
+ultimo_valor = lista.pop(5)
+print (lista, 'Removido', ultimo_valor)
